34923c67d1df60d2ded7960dd63cb5e7be329e29
JoaquinF <joaquinpfelix@gmail.com>
2026-03-01 17:43:31 -0800

    Check 3

--- 210-Lab-15.cpp
+++ 210-Lab-15.cpp
@@ -35,3 +35,43 @@ int main()
 
 }
 
+//openFile() opens input file and checks for errors
+//arguments: ifstream reference
+// returns: true if successful, false otherwise
+bool openFile(ifstream &inFile)
+{
+    inFile.open(INPUT_FILE);
+
+    if (!inFile)
+    {
+        cout << "Error: Could not open file "
+             << INPUT_FILE << "." << endl;
+        return false;
+    }
+    return true;
+}
+
+//LoadMovies() reads file data into vector
+// arguments: vector<Movie> reference
+// returns: nothing
+void loadMovies(vector<Movie> &movie)
+{
+    ifstream inFile;
+
+    if(!openFile(inFile))
+    {
+        return;
+    }
+
+    string title;
+    int year;
+    string writer;
+
+    while (getline(inFile, title))
+    {
+        inFile >> year;
+        inFile.ignore();
+        getline(inFile, writer);
+    }
+
+}
