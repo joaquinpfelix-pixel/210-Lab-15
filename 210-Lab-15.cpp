da804f2c64ddcd56feeb059eb95deddc53e01512
JoaquinF <joaquinpfelix@gmail.com>
2026-03-01 17:22:47 -0800

    Check 1

---
+++ 210-Lab-15.cpp
@@ -0,0 +1,38 @@
+//COMSC-210 | Lab 15 | Joaquin Felix
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+//Constant declarations
+const string INPUT_FILE = "input.txt";
+
+//Movie Class declaration
+class Movie
+{
+    private:
+        string title;
+        int yearReleased;
+        string screenWriter;
+    public:
+    void setTitle(string t);
+    void setYearReleased(int y);
+    void setScreenWriter(string s);
+
+    string getTitle() const;
+    int getYearRelease() const;
+    string getScreenWriter() const;
+
+    void print() const;
+};
+
+//Function prototypes
+bool openFiles(ifstream &inFile);
+void loadMovies(vector<Movie> &movie);
+
+int main()
+{
+
+}
+
+bool
