dcb86747a0f5f5ac2a9de9eda7fa4fac8f4ff97d
JoaquinF <joaquinpfelix@gmail.com>
2026-03-01 18:04:22 -0800

    Check 5

--- 210-Lab-15.cpp
+++ 210-Lab-15.cpp
@@ -20,7 +20,7 @@ class Movie
     void setScreenWriter(string s);
 
     string getTitle() const;
-    int getYearRelease() const;
+    int getYearReleased() const;
     string getScreenWriter() const;
 
     void print() const;
@@ -32,7 +32,15 @@ void loadMovies(vector<Movie> &movie);
 
 int main()
 {
+    vector<Movie> movies;
 
+    loadMovies(movies);
+
+    for (int i = 0; i < movies.size(); i++)
+    {
+        movies[i].print();
+    }
+    return 0;
 }
 
 //openFile() opens input file and checks for errors
@@ -54,7 +62,7 @@ bool openFile(ifstream &inFile)
 //LoadMovies() reads file data into vector
 // arguments: vector<Movie> reference
 // returns: nothing
-void loadMovies(vector<Movie> &movie)
+void loadMovies(vector<Movie> &movies)
 {
     ifstream inFile;
 
@@ -72,6 +80,57 @@ void loadMovies(vector<Movie> &movie)
         inFile >> year;
         inFile.ignore();
         getline(inFile, writer);
-    }
 
+        Movie tempMovie;
+
+        tempMovie.setTitle(title);
+        tempMovie.setYearReleased(year);
+        tempMovie.setScreenWriter(writer);
+
+        movies.push_back(tempMovie);
+    }
+    inFile.close();
+
+}
+
+//setTitle() sets movie title
+void Movie::setTitle(string t)
+{
+    title = t;
+}
+
+//setYearReleased() sets year 
+void Movie::setYearReleased(int y)
+{
+    yearReleased = y;
+}
+//setYearReleased() new year
+void Movie::setScreenWriter(string s)
+{
+    screenWriter = s;
+}
+
+string Movie::getTitle() const
+{
+    return title;
+}
+
+int Movie::getYearReleased() const
+{
+    return yearReleased;
+}
+
+string Movie::getScreenWriter() const
+{
+    return screenWriter;
+}
+
+//print() outputs formatted movie data
+void Movie::print() const
+{
+    cout << "Movie: " << screenWriter << endl;
+    cout << "    Year released: "
+         << yearReleased << endl;
+    cout << "    Screenwriter: "
+         << title << endl << endl;
 }
